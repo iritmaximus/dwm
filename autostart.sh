f74131f18e8c86aedd2e013c29a4a4b156eacc18
iritmaximus <aaro.yliayho@gmail.com>
2022-04-21 15:32:23 +0300

    fixing github fork history things

---
+++ autostart.sh
@@ -0,0 +1,5 @@
+picom --experimental-backends --backend glx -b
+wal -i ~/.wallpapers
+.config/polybar/launch.sh --hack
+dunst
+mpd ~/.config/mpd/mpd.conf
--- dwm.c
+++ dwm.c
@@ -29,6 +29,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h> /* autostart patch */
 #include <sys/wait.h>
 #include <X11/cursorfont.h>
 #include <X11/keysym.h>
@@ -52,8 +53,11 @@
 #define CLEANMASK(mask)         (mask & ~(numlockmask|LockMask) & (ShiftMask|ControlMask|Mod1Mask|Mod2Mask|Mod3Mask|Mod4Mask|Mod5Mask))
 #define GETINC(X)               ((X) - 2000)
 #define INC(X)                  ((X) + 2000)
-#define INTERSECT(x,y,w,h,m)    (MAX(0, MIN((x)+(w),(m)->wx+(m)->ww) - MAX((x),(m)->wx)) \
-                               * MAX(0, MIN((y)+(h),(m)->wy+(m)->wh) - MAX((y),(m)->wy)))
+
+/* Polybar patch */
+#define INTERSECT(x,y,w,h,m)    (MAX(0, MIN((x)+(w),(m)->mx+(m)->mw) - MAX((x),(m)->mx)) \
+                               * MAX(0, MIN((y)+(h),(m)->my+(m)->mh) - MAX((y),(m)->my)))
+
 #define ISINC(X)                ((X) > 1000 && (X) < 3000)
 #define ISVISIBLE(C)            ((C->tags & C->mon->tagset[C->mon->seltags]) || C->issticky)
 #define PREVSEL                 3000
@@ -130,7 +134,11 @@ struct Monitor {
 	float mfact;
 	int nmaster;
 	int num;
-	int by;               /* bar geometry */
+
+/* Polybar patch */
+	int by, bh;           /* bar geometry */
+	int tx, tw;           /* bar tray geometry */
+
 	int mx, my, mw, mh;   /* screen size */
 	int wx, wy, ww, wh;   /* window area  */
 	int gappih;           /* horizontal gap between windows */
@@ -147,6 +155,8 @@ struct Monitor {
 	Client *stack;
 	Monitor *next;
 	Window barwin;
+/* Polybar patch */
+  Window traywin;
 	const Layout *lt[2];
 };
 
@@ -213,6 +223,8 @@ static void incnmaster(const Arg *arg);
 static void keypress(XEvent *e);
 static void killclient(const Arg *arg);
 static void manage(Window w, XWindowAttributes *wa);
+static void managealtbar(Window win, XWindowAttributes *wa);  /* Polybar patch */
+static void managetray(Window win, XWindowAttributes *wa);    /* Polybar patch */
 static void mappingnotify(XEvent *e);
 static void maprequest(XEvent *e);
 static void monocle(Monitor *m);
@@ -229,9 +241,10 @@ static void resizeclient(Client *c, int x, int y, int w, int h);
 static void resizemouse(const Arg *arg);
 static void restack(Monitor *m);
 static void run(void);
-static void runAutostart(void);
+static void runautostart(void); /* autostart patch */
 static void scan(void);
-static int sendevent(Client *c, Atom proto);
+static void scantray(void); /* Polybar patch */
+static int  sendevent(Client *c, Atom proto);
 static void sendmon(Client *c, Monitor *m);
 static void setclientstate(Client *c, long state);
 static void setfocus(Client *c);
@@ -243,13 +256,14 @@ static void seturgent(Client *c, int urg);
 static void showhide(Client *c);
 static void sigchld(int unused);
 #ifndef __OpenBSD__
-static int getdwmblockspid();
+static int  getdwmblockspid();
 static void sigdwmblocks(const Arg *arg);
 #endif
 static void sighup(int unused);
 static void sigterm(int unused);
 static void spawn(const Arg *arg);
-static int stackpos(const Arg *arg);
+static void spawnbar(); /* Polybar patch */
+static int  stackpos(const Arg *arg);
 static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
 static void togglebar(const Arg *arg);
@@ -261,11 +275,13 @@ static void toggletag(const Arg *arg);
 static void toggleview(const Arg *arg);
 static void unfocus(Client *c, int setfocus);
 static void unmanage(Client *c, int destroyed);
+static void unmanagealtbar(Window w); /* Polybar patch */
+static void unmanagetray(Window w);   /* Polybar patch */
 static void unmapnotify(XEvent *e);
 static void updatebarpos(Monitor *m);
 static void updatebars(void);
 static void updateclientlist(void);
-static int updategeom(void);
+static int  updategeom(void);
 static void updatenumlockmask(void);
 static void updatesizehints(Client *c);
 static void updatestatus(void);
@@ -275,6 +291,7 @@ static void updatewmhints(Client *c);
 static void view(const Arg *arg);
 static Client *wintoclient(Window w);
 static Monitor *wintomon(Window w);
+static int wmclasscontains(Window win, const char *class, const char *name); /* Polybar patch */
 static int xerror(Display *dpy, XErrorEvent *ee);
 static int xerrordummy(Display *dpy, XErrorEvent *ee);
 static int xerrorstart(Display *dpy, XErrorEvent *ee);
@@ -290,7 +307,11 @@ static pid_t winpid(Window w);
 
 
 /* variables */
+static const char autostartblocksh[] = "autostart_blocking.sh"; /* autostart patch */
+static const char autostartsh[] = "autostart.sh"; /* autostart patch */
 static const char broken[] = "broken";
+static const char dwmdir[] = "dwm";               /* autostart patch */
+static const char localshare[] = ".local/share";  /* autostart patch */
 static char stext[256];
 static char rawstext[256];
 static int dwmblockssig;
@@ -652,8 +673,11 @@ cleanupmon(Monitor *mon)
 		for (m = mons; m && m->next != mon; m = m->next);
 		m->next = mon->next;
 	}
-	XUnmapWindow(dpy, mon->barwin);
-	XDestroyWindow(dpy, mon->barwin);
+
+  if (!usealtbar) {
+		XUnmapWindow(dpy, mon->barwin);
+		XDestroyWindow(dpy, mon->barwin);
+	}
 	free(mon);
 }
 
@@ -715,7 +739,7 @@ configurenotify(XEvent *e)
 				for (c = m->clients; c; c = c->next)
 					if (c->isfullscreen)
 						resizeclient(c, m->mx, m->my, m->mw, m->mh);
-				XMoveResizeWindow(dpy, m->barwin, m->wx, m->by, m->ww, bh);
+ 				XMoveResizeWindow(dpy, m->barwin, m->wx, m->by, m->ww, m->bh); /* Polybar patch */
 			}
 			focus(NULL);
 			arrange(NULL);
@@ -799,6 +823,7 @@ createmon(void)
 	m->nmaster = nmaster;
 	m->showbar = showbar;
 	m->topbar = topbar;
+  m->bh = bh;         /* Polybar patch */
 	m->gappih = gappih;
 	m->gappiv = gappiv;
 	m->gappoh = gappoh;
@@ -813,6 +838,7 @@ void
 destroynotify(XEvent *e)
 {
 	Client *c;
+  Monitor *m; /* Polybar patch */
 	XDestroyWindowEvent *ev = &e->xdestroywindow;
 
 	if ((c = wintoclient(ev->window)))
@@ -820,6 +846,12 @@ destroynotify(XEvent *e)
 
 	else if ((c = swallowingclient(ev->window)))
 		unmanage(c->swallowing, 1);
+
+  /* Polybar patch */
+  else if ((m = wintomon(ev->window)) && m->barwin == ev->window)
+		unmanagealtbar(ev->window);
+	else if (m->traywin == ev->window)
+		unmanagetray(ev->window);
 }
 
 void
@@ -863,6 +895,10 @@ dirtomon(int dir)
 void
 drawbar(Monitor *m)
 {
+  /* Polybar patch */
+  if (usealtbar)
+    return;
+
 	int x, w, tw = 0;
 	int boxs = drw->fonts->h / 9;
 	int boxw = drw->fonts->h / 6 + 2;
@@ -1260,6 +1296,50 @@ manage(Window w, XWindowAttributes *wa)
 	focus(NULL);
 }
 
+
+/* Polybar patch */
+void
+managealtbar(Window win, XWindowAttributes *wa)
+{
+	Monitor *m;
+	if (!(m = recttomon(wa->x, wa->y, wa->width, wa->height)))
+		return;
+
+	m->barwin = win;
+	m->by = wa->y;
+	bh = m->bh = wa->height;
+	updatebarpos(m);
+	arrange(m);
+	XSelectInput(dpy, win, EnterWindowMask|FocusChangeMask|PropertyChangeMask|StructureNotifyMask);
+	XMoveResizeWindow(dpy, win, wa->x, wa->y, wa->width, wa->height);
+	XMapWindow(dpy, win);
+	XChangeProperty(dpy, root, netatom[NetClientList], XA_WINDOW, 32, PropModeAppend,
+		(unsigned char *) &win, 1);
+}
+
+
+/* POlybar patch */
+void
+managetray(Window win, XWindowAttributes *wa)
+{
+	Monitor *m;
+	if (!(m = recttomon(wa->x, wa->y, wa->width, wa->height)))
+		return;
+
+	m->traywin = win;
+	m->tx = wa->x;
+	m->tw = wa->width;
+	updatebarpos(m);
+	arrange(m);
+	XSelectInput(dpy, win, EnterWindowMask|FocusChangeMask|PropertyChangeMask|StructureNotifyMask);
+	XMoveResizeWindow(dpy, win, wa->x, wa->y, wa->width, wa->height);
+	XMapWindow(dpy, win);
+	XChangeProperty(dpy, root, netatom[NetClientList], XA_WINDOW, 32, PropModeAppend,
+			(unsigned char *) &win, 1);
+}
+
+
+
 void
 mappingnotify(XEvent *e)
 {
@@ -1280,7 +1360,10 @@ maprequest(XEvent *e)
 		return;
 	if (wa.override_redirect)
 		return;
-	if (!wintoclient(ev->window))
+  /* Polybar patch */
+	if (wmclasscontains(ev->window, altbarclass, ""))
+		managealtbar(ev->window, &wa);
+	else if (!wintoclient(ev->window))
 		manage(ev->window, &wa);
 }
 
@@ -1589,8 +1672,81 @@ run(void)
 }
 
 void
-runAutostart(void) {
-	system("killall -q dwmblocks; dwmblocks &");
+runautostart(void)
+{
+  char *pathpfx;
+  char *path;
+  char *xdgdatahome;
+  char *home;
+    struct stat sb;
+
+  if ((home = getenv("HOME")) == NULL)
+    /* this is almost impossible */
+    return;
+
+  /* if $XDG_DATA_HOME is set and not empty, use $XDG_DATA_HOME/dwm
+   * otherwise use ~/.local/share/dwm as autostart script directory
+   */
+
+  xdgdatahome = getenv("XDG_DATA_HOME");
+  if (xdgdatahome != NULL && *xdgdatahome != '\0') {
+      /* space for path segments, separators and nul */
+      pathpfx = ecalloc(1, strlen(xdgdatahome) + strlen(dwmdir) + 2);
+
+      if (sprintf(pathpfx, "%s/%s", xdgdatahome, dwmdir) <= 0) {
+        free(pathpfx);
+        return;
+      }
+  } else {
+    /* space for path segments, separators and nul */
+    pathpfx = ecalloc(1, strlen(home) + strlen(localshare)
+        + strlen(dwmdir) + 3);
+    if (sprintf(pathpfx, "%s/%s/%s", home, localshare, dwmdir) < 0) {
+      free(pathpfx);
+      return;
+    }
+  }
+
+  /* check if the autostart script directory exists */
+  if (! (stat(pathpfx, &sb) == 0 && S_ISDIR(sb.st_mode))) {
+    /* the XDG conformant path does not exist or is no directory
+     * so we try ~/.dwm instead
+    */
+    char *pathpfx_new = realloc(pathpfx, strlen(home) + strlen(dwmdir) + 3);
+    if(pathpfx_new == NULL) {
+      free(pathpfx);
+      return;
+    }
+    pathpfx = pathpfx_new;
+
+    if (sprintf(pathpfx, "%s/.%s", home, dwmdir) <= 0) {
+      free(pathpfx);
+      return;
+    }
+  }
+
+  /* try the blocking script first */
+  path = ecalloc(1, strlen(pathpfx) + strlen(autostartblocksh) + 2);
+  if (sprintf(path, "%s/%s", pathpfx, autostartblocksh) <= 0) {
+    free(path);
+    free(pathpfx);
+  }
+
+  if (access(path, X_OK) == 0)
+    system(path);
+
+  /* now the non-blocking script */
+  if (sprintf(path, "%s/%s", pathpfx, autostartsh) <= 0) {
+    free(path);
+    free(pathpfx);
+  }
+
+  if (access(path, X_OK) == 0)
+    system(strcat(path, " &"));
+
+  free(pathpfx);
+  free(path);
+
 }
 
 void
@@ -1605,8 +1761,13 @@ scan(void)
 			if (!XGetWindowAttributes(dpy, wins[i], &wa)
 			|| wa.override_redirect || XGetTransientForHint(dpy, wins[i], &d1))
 				continue;
-			if (wa.map_state == IsViewable || getstate(wins[i]) == IconicState)
-				manage(wins[i], &wa);
+
+      /* Polybar patch */
+			if (wmclasscontains(wins[i], altbarclass, ""))
+				managealtbar(wins[i], &wa);
+			else if (wa.map_state == IsViewable || getstate(wins[i]) == IconicState)
+        manage(wins[i], &wa);
+
 		}
 		for (i = 0; i < num; i++) { /* now the transients */
 			if (!XGetWindowAttributes(dpy, wins[i], &wa))
@@ -1619,6 +1780,30 @@ scan(void)
 			XFree(wins);
 	}
 }
+
+
+/* Polybar patch */
+void
+scantray(void)
+{
+	unsigned int num;
+	Window d1, d2, *wins = NULL;
+	XWindowAttributes wa;
+
+	if (XQueryTree(dpy, root, &d1, &d2, &wins, &num)) {
+		for (unsigned int i = 0; i < num; i++) {
+			if (wmclasscontains(wins[i], altbarclass, alttrayname)) {
+				if (!XGetWindowAttributes(dpy, wins[i], &wa))
+					break;
+				managetray(wins[i], &wa);
+			}
+		}
+	}
+
+	if (wins)
+		XFree(wins);
+}
+
 
 void
 sendmon(Client *c, Monitor *m)
@@ -1791,7 +1976,7 @@ setup(void)
 	if (!drw_fontset_create(drw, fonts, LENGTH(fonts)))
 		die("no fonts could be loaded.");
 	lrpad = drw->fonts->h;
-	bh = drw->fonts->h + 2;
+	bh = usealtbar ? 0 : drw->fonts->h + 2; /* Polybar patch */
 	updategeom();
 	/* init atoms */
 	utf8string = XInternAtom(dpy, "UTF8_STRING", False);
@@ -1840,6 +2025,7 @@ setup(void)
 	XSelectInput(dpy, root, wa.event_mask);
 	grabkeys();
 	focus(NULL);
+	spawnbar(); /* Polybar patch */
 }
 
 
@@ -1933,6 +2119,15 @@ spawn(const Arg *arg)
 	}
 }
 
+
+/* Polybar patch */
+void
+spawnbar()
+{
+	if (*altbarcmd)
+		system(altbarcmd);
+}
+
 void
 tag(const Arg *arg)
 {
@@ -1954,10 +2149,24 @@ tagmon(const Arg *arg)
 void
 togglebar(const Arg *arg)
 {
+
+  /* Polybar patch */
+  /*
+   * Polybar tray does not raise maprequest event. It must be manually scanned
+   * for. Scanning it too early while the tray is being populated would give
+   * wrong dimensions.
+   */
+  if (!selmon->traywin)
+    scantray();
+
+
+
 	selmon->showbar = !selmon->showbar;
 	updatebarpos(selmon);
-	XMoveResizeWindow(dpy, selmon->barwin, selmon->wx, selmon->by, selmon->ww, bh);
-	arrange(selmon);
+  /* Polybar patch */
+  XMoveResizeWindow(dpy, selmon->barwin, selmon->wx, selmon->by, selmon->ww, selmon->bh);
+  XMoveResizeWindow(dpy, selmon->traywin, selmon->tx, selmon->by, selmon->tw, selmon->bh);
+  arrange(selmon);
 }
 
 void
@@ -2098,10 +2307,48 @@ unmanage(Client *c, int destroyed)
 	}
 }
 
+
+/*Polybar patch */
+void
+unmanagealtbar(Window w)
+{
+    Monitor *m = wintomon(w);
+
+    if (!m)
+        return;
+
+    m->barwin = 0;
+    m->by = 0;
+    m->bh = 0;
+    updatebarpos(m);
+    arrange(m);
+}
+
+
+/* Polybar patch */
+void
+unmanagetray(Window w)
+{
+	Monitor *m = wintomon(w);
+
+	if (!m)
+		return;
+
+	m->traywin = 0;
+	m->tx = 0;
+	m->tw = 0;
+	updatebarpos(m);
+	arrange(m);
+}
+
+
+
+
 void
 unmapnotify(XEvent *e)
 {
 	Client *c;
+  Monitor *m; /* Polybar patch */
 	XUnmapEvent *ev = &e->xunmap;
 
 	if ((c = wintoclient(ev->window))) {
@@ -2109,12 +2356,21 @@ unmapnotify(XEvent *e)
 			setclientstate(c, WithdrawnState);
 		else
 			unmanage(c, 0);
-	}
+  /* Polybar patch */
+	} else if ((m = wintomon(ev->window)) && m->barwin == ev->window)
+		unmanagealtbar(ev->window);
+	else if (m->traywin == ev->window)
+		unmanagetray(ev->window);
 }
 
 void
 updatebars(void)
 {
+
+	if (usealtbar)
+		return;
+
+
 	Monitor *m;
 	XSetWindowAttributes wa = {
 		.override_redirect = True,
@@ -2140,11 +2396,11 @@ updatebarpos(Monitor *m)
 	m->wy = m->my;
 	m->wh = m->mh;
 	if (m->showbar) {
-		m->wh -= bh;
+		m->wh -= m->bh; /* Polybar patch */
 		m->by = m->topbar ? m->wy : m->wy + m->wh;
-		m->wy = m->topbar ? m->wy + bh : m->wy;
+		m->wy = m->topbar ? m->wy + m->bh : m->wy; /* Polybar patch */
 	} else
-		m->by = -bh;
+		m->by = -m->bh; /* Polybar patch */
 }
 
 void
@@ -2487,12 +2743,37 @@ wintomon(Window w)
 	if (w == root && getrootptr(&x, &y))
 		return recttomon(x, y, 1, 1);
 	for (m = mons; m; m = m->next)
-		if (w == m->barwin)
+		if (w == m->barwin || w == m->traywin) /* Polybar patch */
 			return m;
 	if ((c = wintoclient(w)))
 		return c->mon;
 	return selmon;
 }
+
+
+/* Polybar patch */
+int
+wmclasscontains(Window win, const char *class, const char *name)
+{
+	XClassHint ch = { NULL, NULL };
+	int res = 1;
+
+	if (XGetClassHint(dpy, win, &ch)) {
+		if (ch.res_name && strstr(ch.res_name, name) == NULL)
+			res = 0;
+		if (ch.res_class && strstr(ch.res_class, class) == NULL)
+			res = 0;
+	} else
+		res = 0;
+
+	if (ch.res_class)
+		XFree(ch.res_class);
+	if (ch.res_name)
+		XFree(ch.res_name);
+
+	return res;
+}
+
 
 /* There's no way to check accesses to destroyed windows, thus those cases are
  * ignored (especially on UnmapNotify's). Other types of errors call Xlibs
@@ -2620,7 +2901,7 @@ main(int argc, char *argv[])
 		die("pledge");
 #endif /* __OpenBSD__ */
 	scan();
-	runAutostart();
+	runautostart();
 	run();
 	if(restart) execvp(argv[0], argv);
 	cleanup();
